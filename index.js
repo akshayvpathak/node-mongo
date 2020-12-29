eab413bfd55f63feb49a18c771911cea1992aacb
pathakakshay <pathakakshay1111@gmail.com>
2020-12-29 20:08:21 +0530

    Node Mongodb Example 2

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbopr = require('./operations');
+
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 MongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
     console.log('Connected correctly to Server');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
-    collection.insertOne({"name":"Uthappizza","description":"testing"},(err,result)=>{
-        assert.equal(err,null);
-        console.log('After Insert/');
-        console.log(result.ops);
+    
 
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-            console.log('Found:\n');
-            console.log(docs);
-            db.dropCollection('dishes',(err,result)=>{
-                assert.equal(err,null);
-                client.close();
+    dbopr.insertDocument(db,{name: "vadapav", description: "test"},'dishes',(result)=>{
+        
+        console.log('Insert Document:\n' ,result.ops);
+
+        dbopr.findDocuments(db,'dishes',(docs)=>{
+            
+            console.log('Found Document:\n',docs);
+
+            dbopr.updateDocument(db,{name: 'vadapav'},{description: 'updated test'},'dishes',(result)=>{
+                
+                console.log('Updated Document:\n',result.result);
+
+                dbopr.findDocuments(db,'dishes',(docs)=>{
+            
+                    console.log('Found Document:\n',docs);
+                    db.dropCollection("dishes",(result)=>{
+                        console.log("Dropped Collection",result);
+                        client.close();
+                    })
+                    
+                });
             });
-        });
-    });
+        })
+    })
 })
